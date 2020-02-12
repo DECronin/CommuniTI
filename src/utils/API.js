0f46289b47eafd16292b9215894131e92d128dc6
Dana Cronin <cronin43224235@gmail.com>
2020-02-11 23:25:38 -0500

    initial file and folder creations

---
+++ src/utils/API.js
@@ -0,0 +1,14 @@
+export default {
+    GET: () => {
+        return X
+    },
+    UPDATE: () => {
+        return X
+    },
+    CREATE: () => {
+        return X
+    },
+    DELETE: () => {
+        return X
+    },
+}
