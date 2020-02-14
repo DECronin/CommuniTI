d61d3e8677829359e233f75e5326bf4073ba6db5
Dana Cronin <cronin43224235@gmail.com>
2020-02-13 23:00:13 -0500

    center focus navigation switch statement

---
+++ src/components/ActiveThread/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ActiveThread(){
+
+    return(<>
+        <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
+        <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
+        <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
+    </>)
+}
+
+export default ActiveThread;
--- src/components/CenterFocus/index.js
+++ src/components/CenterFocus/index.js
@@ -1,38 +1,56 @@
 // import React { useContext } from "react";
-import React from 'react';
+import React, {Component} from 'react';
 // import FlowContext from "../../utils/flowContext";
+import ActiveThread from "../ActiveThread";
+// import ListThreadLinks from "../ActiveThread";
+// import NewTopicForm from "../ActiveThread";
+// import NewUserForm from "../ActiveThread";
+// import NewResourceForm from "../ActiveThread";
+// import UserBio from "../UserBio";
+// import NewThreadForm from "../NewThreadForm";
+// import Login from "../ActiveThread";
+// import Settings from "../Settings";
+// import TOS from "../TOS";
 
-function CenterFocus(){
+class CenterFocus extends Component {
     // const { activeCompunents } = useContext(FlowContext);
 
-    return (<>
-        <div className="center-focus col-8 border border-primary">
-            <h3>CenterFocus</h3>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-            <p>cfvgbhnjnvinrei  rginoiwfninri fninwifniw wnr i wif wj iw iw ignow   wnfiwniofnwnfimwognive imwrinrfinfknf erifnowinfkc kwnfikew</p>
-    </div>
-   </>)
+    activeFocusContent = pageName => {
+            console.log(pageName);
+        switch(pageName){
+            case 'comments':
+                return <ActiveThread />
+            // case 'threads':
+            //     return <ListThreadLinks />
+            // case 'resources':
+            //     return <ResourceLibrary />
+            // case 'newTopic':
+            //     return <NewTopicForm />
+            // case 'newUser':
+            //     return <NewUserForm />
+            // case 'newResource':
+            //     return <NewResourceForm />
+            // case 'userBio':
+            //     return <UserBio />
+            // case 'newThread':
+            //     return <NewThreadForm />
+            // case 'Settings':
+            //     return <Settings />
+            // case 'tos':
+            //     return <TOS />
+            // default:
+            //     return <Login />
+        }
+    }
+
+    render(){
+        return (<>
+            <div className="center-focus col-8 border border-primary">
+                <h3>CenterFocus</h3>
+                {this.activeFocusContent('comments')}
+            </div>
+        </>)
+    }
 }
 
 export default CenterFocus
