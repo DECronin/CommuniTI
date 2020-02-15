821cb66721cb514aedeacf60cb849528c3f23727
Dana Cronin <cronin43224235@gmail.com>
2020-02-14 20:23:27 -0500

    psudo?

--- src/components/CommentForm/index.js
+++ src/components/CommentForm/index.js
@@ -1,51 +1,75 @@
-import React, {Component} from 'react';
+import React, { useContext, useEffect } from 'react';
 import NewResourceForm from '../NewResourceForm';
 
-class CommentForm extends Component {
-    resourcesArray = [];
-
-    // rederResourcesList = data => { // pull 'data' from state
-    //     this.resourcesArray.push(<li>
-
-    //     </li>)
-    // };
-
-    render(){
-        return(<ul className="form-wrapper border border-warning" key="new"> 
-            <form>
-                <h5>New Comment</h5>
-                <div className="form-row">
-                    <div className="form-group col-9">
-                        <input className="form-row form-group col-12" name="comment-title" id="comment-title" placeholder="Title Statement"></input>
+function CommentForm() {
+    let resourcesArray = [];
+    
+    function renderResourcesList() {
+        let data = [];
+        if (data.length > 0){
+            data.forEach(el => {
+                resourcesArray.push(<li className="single-resource-wrapper" key={data.indexOf(el)}>
+                    <div className="row res-header">
+                        <div className="col-10">{el.title}</div>
+                        <div className="col-2">Stance</div>
                     </div>
-                    <div className="form-group col-3">
-                        <div className="form-check form-row">
-                            <input className="form-check-input" type="radio" name="stance-range" id="stance-range" value="stance-range" />
-                            <label className="form-check-label form-group col-2" htmlFor="stance-range">Pro</label>
-                            <input type="range" className="form-group col-6" id="res-stance" min="1" max="5" defaultValue="3" />
-                            <label className="form-check-label form-group col-2" htmlFor="stance-range">Con</label>
+                    <div className="row res-body">
+                        <div className="row">
+                            <div className="col-4">Publisher/Sponsor: {el.publisher}</div>
+                            <div className="col-5">Author/Contributor(s): {el.authors}</div>
+                            <div className="col-3">Released: {el.releaseDate}</div>
                         </div>
-                        <div className="form-check form-row">
-                            <input className="form-check-input" type="radio" name="stance-other" id="stance-other" value="thance-other" />
-                            <label className="form-check-label" htmlFor="stance-other">
-                                Other
+                        <div className="row">
+                            <div className="col-4">Category: {el.category}</div>
+                            <div className="col-8">URL: {el.url}</div>
+                        </div>
+                    </div>
+                    <div className="row res-footer col-12">{el.additional}</div>
+                </li>)  
+            });
+            console.log("data " + JSON.stringify(data));
+            //setState:
+            return {resourcesArray}
+        }
+    };
+    // useEffect(() =>{
+    //     renderResourcesList()
+    // }, [activeComponents.dataResourceInputs])
+
+    return (<ul className="form-wrapper border border-warning" key="new">
+        <form>
+            <h5>New Comment</h5>
+            <div className="form-row">
+                <div className="form-group col-9">
+                    <input className="form-row form-group col-12" name="comment-title" id="comment-title" placeholder="Title Statement"></input>
+                </div>
+                <div className="form-group col-3">
+                    <div className="form-check form-row">
+                        <input className="form-check-input" type="radio" name="stance-range" id="stance-range" value="stance-range" />
+                        <label className="form-check-label form-group col-2" htmlFor="stance-range">Pro</label>
+                        <input type="range" className="form-group col-6" id="res-stance" min="1" max="5" defaultValue="3" />
+                        <label className="form-check-label form-group col-2" htmlFor="stance-range">Con</label>
+                    </div>
+                    <div className="form-check form-row">
+                        <input className="form-check-input" type="radio" name="stance-other" id="stance-other" value="thance-other" />
+                        <label className="form-check-label" htmlFor="stance-other">
+                            Other
                             </label>
-                        </div>
                     </div>
                 </div>
-                <div className="form-row">
-                    <textarea className="form-group col-12" rows="4" placeholder="Comment..." name="comment-body" id="comment-body"></textarea>
-                </div>
-                <div className="resource-wrapper">
-                    <ul>
-                        <h6>Resources:</h6>
-                        {this.resourcesArray}
-                        <NewResourceForm source="userComment" />
-                    </ul>
-                </div>
-            </form>    
-        </ul>) 
-    }
+            </div>
+            <div className="form-row">
+                <textarea className="form-group col-12" rows="4" placeholder="Comment..." name="comment-body" id="comment-body"></textarea>
+            </div>
+            <div className="resource-wrapper">
+                <ul>
+                    <h6>Resources:</h6>
+                    {resourcesArray}
+                    <NewResourceForm source="userComment"/>
+                </ul>
+            </div>
+        </form>
+    </ul>)
 }
 
 export default CommentForm
