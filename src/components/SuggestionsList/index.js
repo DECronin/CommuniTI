e0b013599ec8ae3c850caa95da0250f76413dc4e
Dana Cronin <cronin43224235@gmail.com>
2020-02-15 09:02:46 -0500

    KEYS

--- src/components/SuggestionsList/index.js
+++ src/components/SuggestionsList/index.js
@@ -5,21 +5,15 @@ function SuggestionsList(){
         <div className="col-right col-2 border border-danger">
             <header>SuggestionsList</header>
             <ul>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
-                <li>2</li>
+                <li key="1">1</li>
+                <li key="2">1</li>
+                <li key="3">1</li>
+                <li key="4">1</li>
+                <li key="5">1</li>
+                <li key="6">1</li>
+                <li key="7">1</li>
+                <li key="8">1</li>
+                <li key="9">1</li>
             </ul>
         </div>
     </>)
