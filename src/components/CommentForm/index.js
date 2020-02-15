9ef59d44edaa7bd1f5e43ed88da49ea1e02e06ba
Dana Cronin <cronin43224235@gmail.com>
2020-02-15 09:02:09 -0500

    usestate implimented and displaying resources array

--- src/components/CommentForm/index.js
+++ src/components/CommentForm/index.js
@@ -1,42 +1,63 @@
-import React, { useContext, useEffect } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import NewResourceForm from '../NewResourceForm';
+import BuildFormContext from '../../utils/BuildFormContext';
 
 function CommentForm() {
-    let resourcesArray = [];
-    
-    function renderResourcesList() {
-        let data = [];
-        if (data.length > 0){
-            data.forEach(el => {
-                resourcesArray.push(<li className="single-resource-wrapper" key={data.indexOf(el)}>
-                    <div className="row res-header">
-                        <div className="col-10">{el.title}</div>
-                        <div className="col-2">Stance</div>
-                    </div>
-                    <div className="row res-body">
-                        <div className="row">
-                            <div className="col-4">Publisher/Sponsor: {el.publisher}</div>
-                            <div className="col-5">Author/Contributor(s): {el.authors}</div>
-                            <div className="col-3">Released: {el.releaseDate}</div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Category: {el.category}</div>
-                            <div className="col-8">URL: {el.url}</div>
-                        </div>
-                    </div>
-                    <div className="row res-footer col-12">{el.additional}</div>
-                </li>)  
-            });
-            console.log("data " + JSON.stringify(data));
-            //setState:
-            return {resourcesArray}
-        }
+    const [formData, setForm] = useState({
+        pageFocus: 'comments',
+        resourceIndex: 0,
+        dataThreadArray: [],
+        author: '',
+        userId: '',
+        topicID: '',
+        threadID: '',
+        title: '',
+        stance: '',
+        comment: '',
+        displayResources: [],
+        dataResourceInputs: [{"title":"","stance":"3","category":"Legal Text","url":"","authors":"I do not currently know.","publisher":"I do not currently know.","releaseDate":"I do not currently know.","additional":""}],
+        other: '?'
+    })
+
+    function renderResourcesList(data) {
+        console.log(`data======\n\n${JSON.stringify(data)}`)
+        
+        let displayData = data.map(el => <>
+            <li className="single-resource-wrapper" key={data.indexOf(el).toString()}>
+                 <div className="row res-header">
+                     <div className="col-10">{el.title}</div>
+                     <div className="col-2">Stance</div>
+                 </div>
+                 <div className="row res-body">
+                     <div className="row">
+                         <div className="col-4">Publisher/Sponsor: {el.publisher}</div>
+                         <div className="col-5">Author/Contributor(s): {el.authors}</div>
+                         <div className="col-2">Released: {el.releaseDate}</div>
+                     </div>
+                     <div className="row">
+                         <div className="col-4">Category: {el.category}</div>
+                         <div className="col-8">URL: {el.url}</div>
+                     </div>
+                 </div>
+                 <div className="row res-footer col-12">{el.additional}</div>
+                 </li>
+            </>
+        )
+            // console.log("displaData " + JSON.stringify(displayData));
+            // setForm({
+            //     ...formData,
+            //     resourceIndex: formData.resourceIndex++,
+            //     displayResources: displayData
+            // })
+            return displayData
+        // }
     };
     // useEffect(() =>{
-    //     renderResourcesList()
-    // }, [activeComponents.dataResourceInputs])
+    //     renderResourcesList(formData.dataResourceInputs)
+    // }, [formData.dataResourceInputs])
 
     return (<ul className="form-wrapper border border-warning" key="new">
+        <BuildFormContext.Provider value={formData, setForm}>
         <form>
             <h5>New Comment</h5>
             <div className="form-row">
@@ -54,7 +75,7 @@ function CommentForm() {
                         <input className="form-check-input" type="radio" name="stance-other" id="stance-other" value="thance-other" />
                         <label className="form-check-label" htmlFor="stance-other">
                             Other
-                            </label>
+                        </label>
                     </div>
                 </div>
             </div>
@@ -63,12 +84,14 @@ function CommentForm() {
             </div>
             <div className="resource-wrapper">
                 <ul>
-                    <h6>Resources:</h6>
-                    {resourcesArray}
-                    <NewResourceForm source="userComment"/>
+                    <h6>Current Resource(s):</h6>
+                    {renderResourcesList(formData.dataResourceInputs)}
+                    <NewResourceForm source="userComment" formData={formData} setForm={setForm} />
                 </ul>
             </div>
+        <button>Submit</button>
         </form>
+        </BuildFormContext.Provider>
     </ul>)
 }
 
