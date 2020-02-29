cc90283875e4f0b01f271b186e40f813e2b19b09
Dana Cronin <cronin43224235@gmail.com>
2020-02-29 11:07:19 -0500

    removing old debugger code

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 const listRoutes = require("./lists");
 // const userRoutes = require("./user");
 
-// router.route("/list/comment").post((req,res) => {
-//     console.log(`============================\nrouter/api/index.js\n==========================`);
-//     res.json({message: "connect index api router"})
-// })
 // lists routes
 router.use("/list", listRoutes);
 
