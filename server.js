00c28ea28fc0dbb5bae335d328bb09d292008a3f
Dana Cronin <cronin43224235@gmail.com>
2020-02-29 09:13:32 -0500

    removing unused /irrelevant code

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
-// const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// const url = "./client/public/index.html"; // || "./client/build/index.html" 
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,14 +12,6 @@ if (process.env.NODE_ENV === "production") {
 }
 // Add routes, both API and view
 app.use("/", routes);
-// app.use(app.router);
-// routes.initialize(app);
-// Send every request to the React app
-// Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, url));
-// });
-
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
