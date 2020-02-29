4ab0a73581e686c86855e7cf97ca49bdb09c809f
Dana Cronin <cronin43224235@gmail.com>
2020-02-29 11:02:25 -0500

    fixing exported/required controler name

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -1,25 +1,25 @@
 const router = require("express").Router();
-const listController = require("../../controllers/listControl");
+const listControl = require("../../controllers/listsControl");
 
 // // Matches with "/api/list/:thing?"
 // ===================================
 // list topics
-router.route("/topics").get(listController.findTopics)
+router.route("/topics").get(listControl.findTopics)
 // list threads
-router.route("/topic/:id").get(listController.findThreads)
+router.route("/topic/:id").get(listControl.findThreads)
 // new thread
-router.route("/thread").post(listController.newThread)
+router.route("/thread").post(listControl.newThread)
 // list comments
-router.route("/thread/:id").get(listController.findComments)
+router.route("/thread/:id").get(listControl.findComments)
 // new comment
-router.route("/comment").post(listController.newComment)
+router.route("/comment").post(listControl.newComment)
 // update comment
-router.route("/comment/:id").put(listController.updateComment)
+router.route("/comment/:id").put(listControl.updateComment)
 // list resources
-router.route("/resources").get(listController.findResources)
+router.route("/resources").get(listControl.findResources)
 // new resource
-router.route("/resources").post(listController.newResource)
+router.route("/resources").post(listControl.newResource)
 // update resource
-router.route("/resources/:id").put(listController.updateResource)
+router.route("/resources/:id").put(listControl.updateResource)
 
 module.exports = router;
