05ab376cddfa4feb413b5084a6a5dcd39dab70e6
Dana Cronin <cronin43224235@gmail.com>
2020-03-02 19:14:18 -0500

    pull from seeded db and display links

--- client/src/components/TopicsList/index.js
+++ client/src/components/TopicsList/index.js
@@ -1,24 +1,50 @@
-import React from 'react';
-// import API from "../../utils/API";
+import React, { useState, useEffect } from 'react';
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
 
 function TopicsList(){
-    // generateTopicsList = () => {
+    const [topicsData, setTopics] = useState({
+        displayTopicsNav: []
+    })
+    
+    function alphabeticalSort(a, b){
+        const maxLength = a.length > b.length ? b.length : a.length;
+        for (let i = 0; i < maxLength;){
+            let x = a.charCodeAt(i);
+            let y = b.charCodeAt(i);
+            if (x === y){
+                i++
+            } else {
+                return x - y
+            }
+        }
+    }
+    function generateTopicsList(){
+        let temp = []
+        API.findTopics().then(data => {
+            let topics = data.data.sort((a, b) => alphabeticalSort(a.title, b.title));
+            topics.forEach(li => {
+                temp.push(
+                <li className="topic-nav" key={li.id.toString()}>
+                    <Link
+                        to={`/topic/{${li.id}}`}
+                        className={window.location.pathname === `/topic/{${li.id}}` ? "nav-link active" : "nav-link"}>
+                    {li.title}</Link>
+                </li>
+            )})
+            setTopics({displayTopicsNav: temp})
+        })
+    };
 
-    // };
+    useEffect(() => {
+        generateTopicsList()
+    }, [topicsData.displayTopicsNav])
+
     return (<>
         <div className="col-left col-2 border border-danger">
             <header>Topics:</header>
             <ul>
-                <li key="1">1</li>
-                <li key="2">1</li>
-                <li key="3">1</li>
-                <li key="4">1</li>
-                <li key="5">1</li>
-                <li key="6">1</li>
-                <li key="7">1</li>
-                <li key="8">1</li>
-                <li key="9">1</li>
-                {/* {this.generateTopicsList()} */}
+                {topicsData.displayTopicsNav}
             </ul>
             <button>Suggest New Topic</button>
         </div>
