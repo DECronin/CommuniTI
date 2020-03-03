2ca02d4e6135785ae468ef009542ef373cf291ad
Dana Cronin <cronin43224235@gmail.com>
2020-03-02 19:15:59 -0500

    id needed for model // issue found seeding

--- models/topic.js
+++ models/topic.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
     const Topic = sequelize.define("Topics", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
         title: {
             type: DataTypes.STRING,
             unique: false,
