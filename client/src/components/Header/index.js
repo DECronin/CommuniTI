e401be73c23b2637a5d672092543aec8f52bc18a
Dana Cronin <cronin43224235@gmail.com>
2020-03-05 20:37:12 -0500

    col-structure and search bar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,9 +4,10 @@ import Nav from "../Nav";
 
 function Header(){
     return (<>
-        <div className="top-header row border border-success">
+        <div className="top-header row border border-success flex justify-content-center">
             <Nav />
-            <header>HEADER</header>
+            <header className="col-8 justify-content-center"><h1>HEADER</h1></header>
+            <input className="col-2" placeholder="Search"/>
         </div>
     </>)
 }
