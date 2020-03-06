e31626615241b82a7e095381ecc0e3acf928c020
Dana Cronin <cronin43224235@gmail.com>
2020-03-05 20:42:56 -0500

    updating user model for now

--- models/user.js
+++ models/user.js
@@ -1,15 +1,17 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 3;
 
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define("Users", {
         last_name: {
             type: DataTypes.STRING,
             unique: false,
-            allowNull: false
+            // allowNull: false
         },
         first_name: {
             type: DataTypes.STRING,
             unique: false,
-            allowNull: false
+            // allowNull: false
         },
         username: {
             type: DataTypes.STRING,
@@ -19,10 +21,10 @@ module.exports = (sequelize, DataTypes) => {
         email: {
             type: DataTypes.STRING,
             unique: true,
-            allowNull: false,
-            validate: {
-                isEmail: true
-            }
+            // allowNull: false,
+            // validate: {
+            //     isEmail: true
+            // }
         },
         password: {
             type: DataTypes.STRING,
@@ -41,8 +43,36 @@ module.exports = (sequelize, DataTypes) => {
         accountStatus: {
             type: DataTypes.STRING,
             unique: false,
-            allowNull: false
+            allowNull: false,
+            defaultValue: 'pending'
         }
     },{underscored: true});
+
+    
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password)
+  }
+
+  function hashPasswordIfChanged(user) {
+    if (user.changed('password')) {
+      return bcrypt.hash(user.get('password'), bcrypt.genSaltSync(saltRounds)).then(hashedPw => {
+        user.password = hashedPw
+      })
+    }
+    return 0
+  }
+
+  // This shoudl trigger if individualHooks set to true on Update or Create
+  User.beforeSave(hashPasswordIfChanged)
+
+//  Pulled from Dan's Example? // eslint-disable-next-line no-unused-vars
+//   User.associate = function(models) {
+//     User.hasMany(models.Address, { foreignKey: 'userId' })
+//     User.hasMany(models.ChangeRequest, { foreignKey: 'userId' })
+//     User.hasOne(models.PractitionerSearchList, { foreignKey: 'userId' })
+//     User.belongsToMany(models.Enrollments, { through: 'UserEnrollments', foreignKey: 'userId' })
+//     User.hasMany(models.Token, { foreignKey: 'userId' })
+//     User.hasMany(models.Certificate, { foreignKey: 'userId' })
+//   }
     return User;
 };
