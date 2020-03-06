70b225ceef10e7686db0be175a0bd74c7ea84161
Dana Cronin <cronin43224235@gmail.com>
2020-03-06 17:26:26 -0500

    router validation and id grab from url

--- client/src/components/ActiveThread/index.js
+++ client/src/components/ActiveThread/index.js
@@ -2,46 +2,52 @@ import React, { useState, useEffect } from 'react';
 import CommentForm from '../CommentForm';
 import API from '../../utils/API';
 
+const htmlTest = "<!DOCTYPE html>\n<html lang=\"en\">\n  <head>\n    <meta charset=\"utf-8\" />\n    <link rel=\"icon\" href=\"%PUBLIC_URL%/favicon.ico\" />\n    <meta name=\"viewport\" content=\"width=device-width";
+
 function ActiveThread () {
     const [displayComments, setDisplay] = useState({
-        list: [],
-        needsUpdated: false
+        list: []
     })
 
     // API Call to fina all comments for this thread-id
     useEffect(() => {
-        API.findComments('1').then(results => {
-            // console.log(`api results:: \n ${JSON.stringify(results.data)}`);
-            renderThreadArray(results.data)
+        const hrefArray = window.location.href.split("/");
+        const threadID = hrefArray[hrefArray.length-1];
+        console.log("thread ID::\n" + threadID);
+        API.findComments('1').then(result => {
+            let middle = result.data.toString().split(",");
+            let obj = (middle[0] === htmlTest) ? {err: 'HTML?'} : result
+            if(obj.data){
+                renderThreadArray(obj.data)
+            }
         })
-    }, [displayComments.needsUpdated])
+    }, [window.location.href])
     
     function renderThreadArray (comments) {
-        let feed = [];
-        // let question = comments.shift();
-        // let feed = [
-        //     <ul className="question-wrapper border border-primary" key={question.id.toString()}>
-        //         <div className="row question-header">
-        //             <div className="username col-10">{question.title}</div>
-        //             <div className="stance col-2">{question.stance}</div>
-        //         </div>
-        //         <div className="question-body row">
-        //             <p>{question.summary}</p>
-        //         </div>
-        //         <div className="question-footer row">
-        //             <div className="author col-7">{question.author}</div>
-        //             <div className="updated-date col-4">{question.updateDate}</div>
-        //             {/* / if user === logedin user then "edit or delete" button / */}
-        //             <button className="report-button">Report</button>
-        //         </div>
-        //         <div className="question-resources row">
-        //             <ul key={`res-${question.id}`}>
-        //                 <li key="1">Populate Resources</li>
-        //             </ul>
-        //         </div><br></br>
-        //     </ul>];
+        // let feed = [];
+        let question = comments.shift();
+        let feed = [
+            <ul className="question-wrapper border border-primary" key={question.id.toString()}>
+                <div className="row question-header">
+                    <div className="username col-10">{question.title}</div>
+                    <div className="stance col-2">{question.stance}</div>
+                </div>
+                <div className="question-body row">
+                    <p>{question.summary}</p>
+                </div>
+                <div className="question-footer row">
+                    <div className="author col-7">{question.author}</div>
+                    <div className="updated-date col-4">{question.updateDate}</div>
+                    {/* / if user === logedin user then "edit or delete" button / */}
+                    <button className="report-button">Report</button>
+                </div>
+                <div className="question-resources row">
+                    <ul key={`res-${question.id}`}>
+                        <li key="1">Populate Resources</li>
+                    </ul>
+                </div><br></br>
+            </ul>];
         comments.forEach(el => {
-            console.log(el)
             feed.push(<>
                 <ul className="comment-wrapper border border-success" key={el.id.toString()}>
                     <div className="row comment-header">
