d199ce9b512ecb6f409a345072b2006b3387edb0
Dana Cronin <cronin43224235@gmail.com>
2020-03-05 20:42:16 -0500

    user login not quite utilized yet but prepared

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,35 +1,39 @@
-// const db = require("../models");
+const db = require("../models");
 
-// module.exports = {
-//     findAll: function (req, res) {
-//         db.User
-//             .find(req.query)
-//             .then(dbModel => res.json(dbModel))
-//             .catch(err => res.status(422).json(err));
-//     },
-//     findById: function (req, res) {
-//         db.User
-//             .findById(req.params.id)
-//             .then(dbModel => res.json(dbModel))
-//             .catch(err => res.status(422).json(err));
-//     },
-//     create: function (req, res) {
-//         db.User
-//             .create(req.body)
-//             .then(dbModel => res.json(dbModel))
-//             .catch(err => res.status(422).json(err));
-//     },
-//     update: function (req, res) {
-//         db.User
-//             .findOneAndUpdate({ _id: req.params.id }, req.body)
-//             .then(dbModel => res.json(dbModel))
-//             .catch(err => res.status(422).json(err));
-//     },
-//     //   remove: function(req, res) {
-//     //     db.User
-//     //       .findById({ _id: req.params.id })
-//     //       .then(dbModel => dbModel.remove())
-//     //       .then(dbModel => res.json(dbModel))
-//     //       .catch(err => res.status(422).json(err));
-//     //   }
-// };
+module.exports = {
+    findAll: function (req, res, next) {
+        console.log(`db =;= models =;=; find all`);
+        db.Users
+            .findAll({where: {username: req.body.username}})
+            .then(dbModel => {
+                console.log(`findAllUsers::\n${req}\n${dbModel}`)
+                // if(dbModel){
+                //     if(dbModel.validPassword(req.body.password)){
+                //         req.user = dbModel;
+                //         next()
+                //     }
+                // }
+            })
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function (req, res) {
+        db.Users
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
+        console.log(`================\nCREATing:new\nreq.body::\n${JSON.stringify(req.body)}\n=================`);
+        // db.Users
+        //     .create(req.body)
+        //     .then(dbModel => res.json(dbModel))
+        //     .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        console.log(`================\nupdating: ${req.params.id}\nreq.body::\n${JSON.stringify(req.body)}\n=================`);
+        // db.Users
+        //     .findOneAndUpdate({ _id: req.params.id }, req.body)
+        //     .then(dbModel => res.json(dbModel))
+        //     .catch(err => res.status(422).json(err));
+    }
+};
