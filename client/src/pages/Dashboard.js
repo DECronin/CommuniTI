3d6f9a1291d66618aeabb8645edbd530acee7845
Dana Cronin <cronin43224235@gmail.com>
2020-03-06 17:28:04 -0500

    separating out switch router // Hash Router make for more consistent if navigaing directly instead of from single session

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,20 +1,10 @@
 import React from "react";
 // import API from "../utils/API";
 import Header from "../components/Header";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom";
 import TopicsList from "../components/TopicsList";
 import SuggestionsList from "../components/SuggestionsList";
-import ActiveThread from "../components/ActiveThread";
-import ListThreadLinks from "../components/ListThreadLinks";
-import NewTopicForm from "../components/NewTopicForm";
-import NewUserForm from "../components/NewUserForm";
-import NewResourceForm from "../components/NewResourceForm";
-import UserBio from "../components/UserBio";
-import NewThreadForm from "../components/NewThreadForm";
-import Login from "../components/Login";
-import Settings from "../components/Settings";
-import TOS from "../components/TOS";
-import ResourcesList from "../components/ResourceLibrary";
+import CenterFocus from "../components/CenterFocus";
 
 function Dashboard() {
 
@@ -25,18 +15,7 @@ function Dashboard() {
                 <TopicsList />
                 {/* CenterFocus */}
                 <div className="center-focus col-8 border border-primary">
-                    <h3>CenterFocus</h3>
-                    <Route exact path="/" component={Login} />
-                    <Route exact path="/thread" component={ActiveThread} />
-                    <Route exact path="/topic" component={ListThreadLinks} />
-                    <Route exact path="/newtopic" component={NewTopicForm} />
-                    <Route exact path="/newresource" component={NewResourceForm} />
-                    <Route exact path="/newthread" component={NewThreadForm} />
-                    <Route exact path="/newuser" component={NewUserForm} />
-                    <Route exact path="/user" component={UserBio} />
-                    <Route exact path="/resources" component={ResourcesList} />
-                    <Route exact path="/termsofservice" component={TOS} />
-                    <Route exact path="/settings" component={Settings} />
+                    <CenterFocus />
                     {/* // 404 page */}
                 </div>
                 {/* CenterFocus */}
