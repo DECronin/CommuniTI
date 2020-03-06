da5116f92b78a0a5616809063beb5c01dff835a7
Dana Cronin <cronin43224235@gmail.com>
2020-03-05 20:36:12 -0500

    condensing from separating out app config

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-const express = require("express");
-const routes = require("./routes");
-const PORT = process.env.PORT || 3001;
-const app = express();
+require('dotenv').config()
+const app = require('./app')
+const db = require('./models')
+const PORT = process.env.PORT || 3001
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use("/", routes);
-
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`API Server now listening on PORT ${PORT}!`)
+    })
+  })
+  .catch(err => console.log(err))
