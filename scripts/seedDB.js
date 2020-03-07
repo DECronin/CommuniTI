2a82ebd26d6af11e2ec03e4ee383be59b2c41cf3
Dana Cronin <cronin43224235@gmail.com>
2020-03-06 21:13:21 -0500

    seeding more into db for each table

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,40 +1,52 @@
 const db = require("../models");
 
-// const commentSeeds = [{
-//     title: "test 1",
-//     summary: "Passport is authentication middleware for Node.js. Extremely flexible and modular, Passport can be unobtrusively dropped in to any Express-based web application. A comprehensive set of strategies support authentication using a username and password, Facebook, Twitter, and more.",
-//     stance: "1"
-// }, {
-//     title: "test 2",
-//     summary: "In modern web applications, authentication can take a variety of forms. Traditionally, users log in by providing a username and password.",
-//     stance: "1"
-// }, {
-//     title: "test 3",
-//     summary: "With the rise of social networking, single sign-on using an OAuth provider such as Facebook or Twitter has become a popular authentication method. Services that expose an API often require token-based credentials to protect access.",
-//     stance: "5"
-// }, {
-//     title: "test 4",
-//     summary: "Despite the complexities involved in authentication, code does not have to be complicated.",
-//     stance: "4"
-// }];
+const commentSeeds = [{
+    title: "test 1",
+    summary: "Passport is authentication middleware for Node.js. Extremely flexible and modular, Passport can be unobtrusively dropped in to any Express-based web application. A comprehensive set of strategies support authentication using a username and password, Facebook, Twitter, and more.",
+    stance: "1",
+    thread_id: '25',
+    user_id: '1'
+}, {
+    title: "test 2",
+    summary: "In modern web applications, authentication can take a variety of forms. Traditionally, users log in by providing a username and password.",
+    stance: "1",
+    thread_id: '25',
+    user_id: '1'
+}, {
+    title: "test 3",
+    summary: "With the rise of social networking, single sign-on using an OAuth provider such as Facebook or Twitter has become a popular authentication method. Services that expose an API often require token-based credentials to protect access.",
+    stance: "5",
+    thread_id: '25',
+    user_id: '1'
+}, {
+    title: "test 4",
+    summary: "Despite the complexities involved in authentication, code does not have to be complicated.",
+    stance: "4",
+    thread_id: '25',
+    user_id: '1'
+}];
 
-// const threadSeeds = [{
-//     title: "THREAD 1",
-//     summary: "Passport is authentication middleware for Node.js. Extremely flexible and modular, Passport can be unobtrusively dropped in to any Express-based web application. A comprehensive set of strategies support authentication using a username and password, Facebook, Twitter, and more.",
-//     stance: "1"
-// }, {
-//     title: "THREAD 2",
-//     summary: "In modern web applications, authentication can take a variety of forms. Traditionally, users log in by providing a username and password.",
-//     stance: "1"
-// }, {
-//     title: "THREAD 3",
-//     summary: "With the rise of social networking, single sign-on using an OAuth provider such as Facebook or Twitter has become a popular authentication method. Services that expose an API often require token-based credentials to protect access.",
-//     stance: "5"
-// }, {
-//     title: "THREAD 4",
-//     summary: "Despite the complexities involved in authentication, code does not have to be complicated.",
-//     stance: "4"
-// }]
+const threadSeeds = [{
+    title: "THREAD 1",
+    summary: "Passport is authentication middleware for Node.js. Extremely flexible and modular, Passport can be unobtrusively dropped in to any Express-based web application. A comprehensive set of strategies support authentication using a username and password, Facebook, Twitter, and more.",
+    stance: "1",
+    topic_id: '1'
+}, {
+    title: "THREAD 2",
+    summary: "In modern web applications, authentication can take a variety of forms. Traditionally, users log in by providing a username and password.",
+    stance: "1",
+    topic_id: '1'
+}, {
+    title: "THREAD 3",
+    summary: "With the rise of social networking, single sign-on using an OAuth provider such as Facebook or Twitter has become a popular authentication method. Services that expose an API often require token-based credentials to protect access.",
+    stance: "5",
+    topic_id: '1'
+}, {
+    title: "THREAD 4",
+    summary: "Despite the complexities involved in authentication, code does not have to be complicated.",
+    stance: "4",
+    topic_id: '1'
+}]
 
 const seedTopics = [
     { title: "Art" },
@@ -77,7 +89,7 @@ const seedTopics = [
     { title: "Health & Medical" },
     { title: "Medicine" },
     { title: "Fashion" },
-    { title: "Stock Market"},
+    { title: "Stock Market" },
     { title: "Animals" },
     { title: "Health Care" },
     { title: "Case Law" },
@@ -87,40 +99,111 @@ const seedTopics = [
 const testUsers = [{
     username: 'test1',
     password: 'test1'
-},{
+}, {
     username: 'test2',
     password: 'test2'
+}];
+
+const seedResources = [{
+    title: "test res 1",
+    category: "Other",
+    url: "https://www.google.com/",
+}, {
+    title: "test res 2",
+    category: "Other",
+    url: "https://www.google.com/"
+}];
+
+const seedCR = [{
+    comment_id: 4,
+    resource_id: 1
+}, {
+    comment_id: 3,
+    resource_id: 1
+}, {
+    comment_id: 2,
+    resource_id: 2
+}, {
+    comment_id: 1,
+    resource_id: 2
+}]
+
+const seedTT = [{
+    thread_id: 1,
+    topic_id: 1
+}, {
+    thread_id: 1,
+    topic_id: 2
+}, {
+    thread_id: 1,
+    topic_id: 5
+}, {
+    thread_id: 1,
+    topic_id: 2
+}, {
+    thread_id: 1,
+    topic_id: 6
+}, {
+    thread_id: 1,
+    topic_id: 2
+}, {
+    thread_id: 4,
+    topic_id: 2
+}, {
+    thread_id: 1,
+    topic_id: 20
 }]
 
 db.sequelize.sync().then(async () => {
     db.Topics.bulkCreate(seedTopics).then(data => {
-        console.log(data.result + "TOPICS records inserted!");
+        console.log(data.result + "TOPICS records inserted!\n");
     })
         .catch(err => {
             console.error(err);
             process.exit(1);
         });
-    // db.Comments.bulkCreate(commentSeeds).then(data => {
-    //     console.log(data.result + " records inserted!");
-    //     process.exit(0);
-    // })
-    //     .catch(err => {
-    //         console.error(err);
-    //         process.exit(1);
-    //     });
-    db.Users.bulkCreate(testUsers, {individualHooks: true}).then(data => {
-            console.log(JSON.stringify(testUsers) + " USERS records inserted!");
+    db.Users.bulkCreate(testUsers, { individualHooks: true }).then(data => {
+        console.log(JSON.stringify(testUsers) + " USERS records inserted!\n");
+    })
+        .catch(err => {
+            console.error(err);
+            process.exit(1);
         })
-            .catch(err => {
-                console.error(err);
-                process.exit(1);
-            })
-    // db.Threads.bulkCreate(threadSeeds).then(data => {
-    //     console.log(data.result + " records inserted!");
-    //     process.exit(0);
-    // })
-    //     .catch(err => {
-    //         console.error(err);
-    //         process.exit(1);
-    //     });
+    db.Comments.bulkCreate(commentSeeds).then(data => {
+        console.log(data.result + " COMMENTS records inserted!\n");
+    })
+        .catch(err => {
+            console.error(err);
+            process.exit(1);
+        });
+    db.Threads.bulkCreate(threadSeeds).then(data => {
+        console.log(data.result + " THREADS records inserted!\n");
+
+        // db.ThreadTopic.bulkCreate(seedTT).then(data => {
+        //     console.log(data.result + " T ==  T records inserted!\n");
+        // })
+        //     .catch(err => {
+        //         console.error(err);
+        //         process.exit(1);
+        //     });
+    })
+        .catch(err => {
+            console.error(err);
+            process.exit(1);
+        });
+    db.Resources.bulkCreate(seedResources).then(data => {
+        console.log(data.result + " RESOURCES records inserted!\n");
+
+        // db.CommentResources.bulkCreate(seedCR).then(data => {
+        //     console.log(data.result + " C ==  R records inserted!\n");
+        // })
+        //     .catch(err => {
+        //         console.error(err);
+        //         process.exit(1);
+        //     });
+    })
+        .catch(err => {
+            console.error(err);
+            process.exit(1);
+        });
 }).catch(err => console.log(err))
