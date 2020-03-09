2a1aec02eecb3baa939ba033801f4cf17036bb97
Dana Cronin <cronin43224235@gmail.com>
2020-03-08 22:53:46 -0400

    local storage

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,27 +3,31 @@ import { Link } from "react-router-dom";
 import API from "../../utils/API";
 import $ from 'jquery';
 
-function LoginForm(){
-    function validate(data){
-        console.log("login form\n\n::" + JSON.stringify(data));
-        API.login(data).then(result => {
-            console.log(`>REDIRECT TO USER BIO OR OTHER CENTER FOCUS<\n\nresult data::\n${JSON.stringify(result)}`);
-            // window.location.href = '/newtopic';
+function LoginForm({updateLogin}){
+    function validate(data, cb){
+        API.login(data).then((result) => {
+            console.log(`result::\n${JSON.stringify(result)}`);
+            localStorage.setItem('id', result.data.id);
+            localStorage.setItem('loggedIn', 'true')
+            cb({
+                loggedIn: true,
+                id: result.data.id
+            })
         })
     }
-    function formObject(e){
+    function formObject(e, cb){
         const inputs = {
             username: $("#username").val(),
             password: $("#password").val()
         }
-        validate(inputs)
+        validate(inputs, cb)
         e.preventDefault()
     }
     return (
     <form className="form-wrapper border border-primary">
             <input name="username" id="username" placeholder="username"></input>
             <input name="password" id="password" placeholder="password"></input>
-            <button onClick={(e) => formObject(e)}>Login</button> 
+            <button onClick={(e) => formObject(e, updateLogin)}>Login</button> 
             <Link to="/newuser">New User</Link> 
             {/* / if source == userComment send to state array for display purposes else send to recommendationPend / */}
     </form> )
