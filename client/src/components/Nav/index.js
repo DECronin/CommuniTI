0912323664f051d08cf2f8c946d858810e40e7b9
Dana Cronin <cronin43224235@gmail.com>
2020-03-08 22:54:00 -0400

    logout

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import API from '../../utils/API';
 
-function Nav(){
+function Nav({loginData}){
+
     // let links;
     // renderLinkOptions(){
-     let links = [<ul>
-            <Link to="/">Home / Login</Link>
-        </ul>,<ul>
+     let links = [<ul key="logout-button">
+            <button id="user-logout" 
+                onClick={() => logOut()}>{loginData.loggedIn ? "Logout" : "Login"}
+            </button>
+        </ul>,<ul key="settings-nav">
             <Link to="/settings">Settings</Link>
-        </ul>,<ul>
+        </ul>,<ul key="tos-nav">
             <Link to="/termsofservice">TOS</Link>
-        </ul>,<ul>
+        </ul>,<ul key="user-bio-nav">
             <Link to="/user">USER BIO</Link>
-        </ul>,<ul>
+        </ul>,<ul key="resource-nav">
             <Link to="/resources">resources</Link>
-        </ul>,<ul>
-            <button>Log{isLoggedIn(true)}</button>
         </ul>]
     // }
+    console.log(window.location.href)
 
     function isLoggedIn(status){
-        //change with context or api-call
+        // filler ~/~ change for more fluid use of {loginData}
         return status ? "IN" : "OUT"
     }
 
+    function logOut(){
+        localStorage.clear()
+        API.logout().then(() => {
+            window.location.href = "/"
+        })
+    }
+
     return (<>
-        <p className="row col-12 border border-danger">~/~ {isLoggedIn(false)}
-        {links}</p>
+        <ul className="row col-12 border border-danger">~/~ {isLoggedIn(loginData.loggedIn)}
+        {links}</ul>
         <h1 className="col-2">NAV</h1>
     </>)
 }
