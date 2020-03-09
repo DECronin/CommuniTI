8ca7fe9b0e5de8a734e49c4f13416a524c213c74
Dana Cronin <cronin43224235@gmail.com>
2020-03-09 18:51:43 -0400

    drilling and condensing

--- client/src/components/TopicsList/index.js
+++ client/src/components/TopicsList/index.js
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from 'react';
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 
-const htmlTest = "<!DOCTYPE html>\n<html lang=\"en\">\n  <head>\n    <meta charset=\"utf-8\" />\n    <link rel=\"icon\" href=\"%PUBLIC_URL%/favicon.ico\" />\n    <meta name=\"viewport\" content=\"width=device-width";
-
-function TopicsList(){
+function TopicsList({loginData}){
     const [topicsData, setTopics] = useState({
         displayTopicsNav: []
     })
@@ -12,22 +10,18 @@ function TopicsList(){
     function alphabeticalSort(a, b){
         const maxLength = a.length > b.length ? b.length : a.length;
         for (let i = 0; i < maxLength;){
-            let x = a.charCodeAt(i);
-            let y = b.charCodeAt(i);
-            if (x === y){
+            if (a.charCodeAt(i) === b.charCodeAt(i)){
                 i++
             } else {
-                return x - y
+                return a.charCodeAt(i) - b.charCodeAt(i)
             }
         }
     }
     function generateTopicsList(){
         let temp = [];
         API.findTopics().then(result => {
-            let middle = result.data.toString().split(",");
-            let obj = (middle[0] === htmlTest) ? {err: 'HTML?'} : result
-            if (obj.data){
-                let topics = obj.data.sort((a, b) => alphabeticalSort(a.title, b.title));
+            if (result.data){
+                let topics = result.data.sort((a, b) => alphabeticalSort(a.title, b.title));
                 topics.forEach(li => {
                     temp.push(
                     <ul className="topic-nav border border-primary" key={li.id.toString()}>
@@ -42,14 +36,23 @@ function TopicsList(){
         })
     };
 
+    function buttonDisplay(status){
+        return(
+            status ?
+            <>
+            <button><Link to="/newtopic">New Topic</Link></button>
+            <button><Link to="/newresource">New Resource</Link></button>
+            </> : ''
+        )
+    }
+
     useEffect(() => {
-        topicsData.displayTopicsNav.length > 1 ? console.log(`persist`) : generateTopicsList()
-    },[window.location.href])
+        generateTopicsList()
+    })
 
     return (<>
         <div className="col-left col-2 border border-danger">
-            <button><Link to="/newtopic">New Topic</Link></button>
-            <button><Link to="/newresource">New Resource</Link></button>
+            {buttonDisplay(loginData.loggedIn)}
             <header>Topics:</header>
             <ul>
                 {topicsData.displayTopicsNav}
