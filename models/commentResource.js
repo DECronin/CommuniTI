d61915aef0237ed9729e3c82d4df038fab1f8132
Dana Cronin <cronin43224235@gmail.com>
2020-03-08 22:56:23 -0400

    hard-coding association tables

--- models/commentResource.js
+++ models/commentResource.js
@@ -5,6 +5,16 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             autoIncrement: true,
             allowNull: false
+        },
+        comment_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'testing'
+        },
+        resource_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'testing'
         }
     },{underscored: true});
     return CommentResource;
--- models/threadTopic.js
+++ models/threadTopic.js
@@ -1,10 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
-    const ThreadTopic = sequelize.define("threadTopics", {
+    const ThreadTopic = sequelize.define("ThreadTopics", {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true,
             allowNull: false
+        },
+        topic_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'testing'
+        },
+        thread_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'testing'
         }
     },{underscored: true});
     return ThreadTopic;
