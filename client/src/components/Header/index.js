07ebee0e55a77ba9e59776170b56c950f4d8497d
Dana Cronin <cronin43224235@gmail.com>
2020-03-08 22:53:33 -0400

    drilling data

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Nav from "../Nav";
 
-function Header(){
+function Header({loginData}){
     return (<>
         <div className="top-header row border border-success flex justify-content-center">
-            <Nav />
+            <Nav loginData={loginData} />
             <header className="col-8 justify-content-center"><h1>HEADER</h1></header>
             <input className="col-2" placeholder="Search"/>
         </div>
