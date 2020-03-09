3d0f1e0e01e87ca2d78a5d2a7573493eea994404
Dana Cronin <cronin43224235@gmail.com>
2020-03-09 18:51:00 -0400

    local storage setup

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -5,13 +5,17 @@ import $ from 'jquery';
 
 function LoginForm({updateLogin}){
     function validate(data, cb){
+        localStorage.clear();
+        console.log(`sending data : ${JSON.stringify(data)}`)
         API.login(data).then((result) => {
-            console.log(`result::\n${JSON.stringify(result)}`);
+            console.log(JSON.stringify(result));
             localStorage.setItem('id', result.data.id);
             localStorage.setItem('loggedIn', 'true')
+            localStorage.setItem('username', data.username)
             cb({
                 loggedIn: true,
-                id: result.data.id
+                id: result.data.id,
+                username: data.username
             })
         })
     }
@@ -27,7 +31,7 @@ function LoginForm({updateLogin}){
     <form className="form-wrapper border border-primary">
             <input name="username" id="username" placeholder="username"></input>
             <input name="password" id="password" placeholder="password"></input>
-            <button onClick={(e) => formObject(e, updateLogin)}>Login</button> 
+            <Link onClick={(e) => formObject(e, updateLogin)} to="/">Login</Link> 
             <Link to="/newuser">New User</Link> 
             {/* / if source == userComment send to state array for display purposes else send to recommendationPend / */}
     </form> )
