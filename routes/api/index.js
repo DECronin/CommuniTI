d5b81fd7e0a72a25520158ad23aa450d67b9d256
Dana Cronin <cronin43224235@gmail.com>
2020-03-09 18:53:18 -0400

    3 /api/user routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const listControl = require("../../controllers/listsControl");
-// const middleAuth = require("../../controllers/middleAuth");
-
-// Matches with "/api/:thing?"
 
 // Public
 // =============
-// FIND (List)
+// FIND
+router.route("/user/:id").get(listControl.findUser)
+// (List)
 router.route("/topics").get(listControl.findTopics)
 router.route("/topic/:id").get(listControl.findThreads)
 router.route("/thread/:id").get(listControl.findComments)
@@ -16,10 +15,12 @@ router.route("/resources").get(listControl.findResources)
 // =============
 // CREATE
 // new user
+router.route("/user").post(listControl.newUser)
 router.route("/thread").post(listControl.newThread)
 router.route("/comment").post(listControl.newComment)
 router.route("/resources").post(listControl.newResource)
 // UPDATE
+router.route("/user/:id").put(listControl.updateUser)
 router.route("/comment/:id").put(listControl.updateComment)
 router.route("/resources/:id").put(listControl.updateResource)
 
