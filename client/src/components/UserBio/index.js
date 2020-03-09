5fe135b38b5da6e1d132ab0bc1cf9652beae6355
Dana Cronin <cronin43224235@gmail.com>
2020-03-09 18:52:00 -0400

    drill username and pseudo prep

--- client/src/components/UserBio/index.js
+++ client/src/components/UserBio/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+// import API from '../../utils/API';
 
-function UserBio() {
+function UserBio({loginData}) {
+
+    // function renderUserInfo(x){
+    //     API.findUser(x.id).then(result => {
+    //         console.log("user bio api call\n=========\n" + result.data[0])
+    //         // populate with information from result.data[0]
+    //     })
+    // };
 
     return (
         <>
             <div>
-                <header> UserBio </header>
+                <header> UserBio: {loginData.username} </header>
                 <div> Options </div>
                 <div> Options </div>
                 <div> Options </div>
