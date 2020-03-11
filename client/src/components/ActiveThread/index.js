4a55f03c034c0df828c03ca3e34b753e10996484
Dana Cronin <cronin43224235@gmail.com>
2020-03-11 00:19:07 -0400

    pull apropriate comments by thread_id

--- client/src/components/ActiveThread/index.js
+++ client/src/components/ActiveThread/index.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import CommentForm from '../CommentForm';
 import API from '../../utils/API';
 
-const htmlTest = "<!DOCTYPE html>\n<html lang=\"en\">\n  <head>\n    <meta charset=\"utf-8\" />\n    <link rel=\"icon\" href=\"%PUBLIC_URL%/favicon.ico\" />\n    <meta name=\"viewport\" content=\"width=device-width";
-
 function ActiveThread ({loginData}) {
     const [displayComments, setDisplay] = useState({
         list: []
@@ -12,65 +10,84 @@ function ActiveThread ({loginData}) {
 
     // API Call to fina all comments for this thread-id
     useEffect(() => {
-        console.log("thread ID::    " + thread_id);
-        API.findComments('1').then(result => {
-            let middle = result.data.toString().split(",");
-            let obj = (middle[0] === htmlTest) ? {err: 'HTML?'} : result
-            if(obj.data){
-                renderThreadArray(obj.data)
-            }
+        console.log("thread ID:: " + thread_id);
+        API.findComments(thread_id).then(result => {
+            setDisplay({list: renderThreadArray(result.data)})
         })
-    }, [window.location.href])
+    }, [thread_id])
     
     function renderThreadArray (comments) {
-        // let feed = [];
-        let question = comments.shift();
-        let feed = [
-            <ul className="question-wrapper border border-primary" key={question.id.toString()}>
-                <div className="row question-header">
-                    <div className="username col-10">{question.title}</div>
-                    <div className="stance col-2">{question.stance}</div>
-                </div>
-                <div className="question-body row">
-                    <p>{question.summary}</p>
-                </div>
-                <div className="question-footer row">
-                    <div className="author col-7">{question.author}</div>
-                    <div className="updated-date col-4">{question.updateDate}</div>
-                    {/* / if user === logedin user then "edit or delete" button / */}
-                    <button className="report-button">Report</button>
-                </div>
-                <div className="question-resources row">
-                    <ul key={`res-${question.id}`}>
-                        <li key="1">Populate Resources</li>
-                    </ul>
-                </div><br></br>
-            </ul>];
-        comments.forEach(el => {
-            feed.push(<>
-                <ul className="comment-wrapper border border-success" key={el.id.toString()}>
-                    <div className="row comment-header">
-                        <div className="username col-10">{el.title}</div>
-                        <div className="stance col-2">{el.stance}</div>
+        let temp = [];
+        if (!comments.length){
+            temp.push(<><ul className="question-wrapper border border-primary" key="testing">
+            <div className="row question-header">
+                <div className="username col-10">title</div>
+                <div className="stance col-2">3</div>
+            </div>
+            <div className="question-body row">
+                <p>summary</p>
+            </div>
+            <div className="question-footer row">
+                <div className="author col-7">author</div>
+                <div className="updated-date col-4">data</div>
+                {/* / if user === logedin user then "edit or delete" button / */}
+                {/* <button className="report-button">Report</button> */}
+            </div>
+            <div className="question-resources row">
+                <ul key="res-1">
+                    <li key="1">Populate Resources</li>
+                </ul>
+            </div><br></br>
+        </ul></>)
+        } else  {
+            let question = comments.shift();
+            temp.push(<>
+                <ul className="question-wrapper border border-primary" key={question.id.toString()}>
+                    <div className="row question-header">
+                        <div className="username col-10">{question.title}</div>
+                        <div className="stance col-2">{question.stance}</div>
                     </div>
-                    <div className="comment-body row">
-                        <p>{el.summary}</p>
+                    <div className="question-body row">
+                        <p>{question.summary}</p>
                     </div>
-                    <div className="comment-footer row">
-                        <div className="author col-7">{el.author}</div>
-                        <div className="updated-date col-4">{el.updateDate}</div>
+                    <div className="question-footer row">
+                        <div className="author col-7">{question.author}</div>
+                        <div className="updated-date col-4">{question.updateDate}</div>
                         {/* / if user === logedin user then "edit or delete" button / */}
                         <button className="report-button">Report</button>
                     </div>
-                    <div className="comment-resources row">
-                        <ul key={`res-${el.id}`}>
-                            <li key={`res-${el.id}`}>Populate Resources</li>
+                    <div className="question-resources row">
+                        <ul key={`res-${question.id}`}>
+                            <li key="1">Populate Resources</li>
                         </ul>
                     </div><br></br>
-                </ul>   
-            </>);
-        });
-        setDisplay({...displayComments, list: feed})
+                </ul></>);
+            comments.forEach(el => {
+                temp.push(<>
+                    <ul className="comment-wrapper border border-success" key={el.id.toString()}>
+                        <div className="row comment-header">
+                            <div className="username col-10">{el.title}</div>
+                            <div className="stance col-2">{el.stance}</div>
+                        </div>
+                        <div className="comment-body row">
+                            <p>{el.summary}</p>
+                        </div>
+                        <div className="comment-footer row">
+                            <div className="author col-7">{el.author}</div>
+                            <div className="updated-date col-4">{el.updateDate}</div>
+                            {/* / if user === logedin user then "edit or delete" button / */}
+                            <button className="report-button">Report</button>
+                        </div>
+                        <div className="comment-resources row">
+                            <ul key={`res-${el.id}`}>
+                                <li key={`res-${el.id}`}>Populate Resources</li>
+                            </ul>
+                        </div><br></br>
+                    </ul>   
+                </>);
+            });
+        }
+        return temp
     };
 
     function renderForm(status){
