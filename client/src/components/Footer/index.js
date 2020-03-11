fef5dec564b3d4740fa1813d14121ed780d1fb2f
Dana Cronin <cronin43224235@gmail.com>
2020-03-11 15:39:38 -0400

    slight footer prep

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function Footer(){
-    return(<footer>
-        FOOTER
+    return(<footer className="row">
+        <p className="col-4"></p>
+        <p className="col-4 d-flex justify-content-center">FOOTER</p>
+        <p className="col-4"></p>
     </footer>)
 }
 
