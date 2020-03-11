f31e8f4c02cfa4d1c4110c140bfd6822657cea7b
Dana Cronin <cronin43224235@gmail.com>
2020-03-10 20:51:32 -0400

    new topic form to come in future dev

--- client/src/components/CenterFocus/index.js
+++ client/src/components/CenterFocus/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import ActiveThread from "../ActiveThread";
 import ListThreadLinks from "../ListThreadLinks";
-import NewTopicForm from "../NewTopicForm";
 import NewUserForm from "../NewUserForm";
 import NewResourceForm from "../NewResourceForm";
 import UserBio from "../UserBio";
@@ -11,19 +10,21 @@ import Login from "../Login";
 import Settings from "../Settings";
 import TOS from "../TOS";
 import ResourcesList from "../ResourceLibrary";
+// import NewTopicForm from "../NewTopicForm";
 
 function CenterFocus({loginData, updateLogin}){
     return(<><h3>CenterFocus</h3><Switch>
         <Route exact path="/" render={() => loginData.loggedIn ? <UserBio loginData={loginData} /> : <Login updateLogin={updateLogin} />} />
         <Route path="/thread/:id" render={() => <ActiveThread loginData={loginData} />} />
         <Route path="/topic/:id" render={() => <ListThreadLinks loginData={loginData} />} />
-        <Route exact path="/newtopic" render={() => <NewTopicForm />} />
         <Route exact path="/newresource" render={() => <NewResourceForm source="reccomendation" />} />
-        <Route exact path="/newthread" render={() => <NewThreadForm />} />
-        <Route exact path="/newuser" render={() => <NewUserForm />} />
+        <Route exact path="/newthread" render={() => <NewThreadForm loginData={loginData} />} />
+        <Route exact path="/newuser" render={() => <NewUserForm updateLogin={updateLogin} />} />
         <Route exact path="/resources" render={() => <ResourcesList loginData={loginData} />} />
-        <Route exact path="/termsofservice" render={() => <TOS />} />
+        <Route exact path="/termsofservice" render={() => <TOS loginData={loginData} />} />
         <Route exact path="/settings" render={() => <Settings loginData={loginData} />} />
+        {/* <Route exact path="/newtopic" render={() => <NewTopicForm />} /> */}
+        {/* replace ^ with search bar results */}
         </Switch></>)
 }
 
