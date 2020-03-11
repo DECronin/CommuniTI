8a3500b2760f3eb8a77260661209e183153b8fbc
Dana Cronin <cronin43224235@gmail.com>
2020-03-10 20:52:08 -0400

    starting nre thread form

--- client/src/components/NewThreadForm/index.js
+++ client/src/components/NewThreadForm/index.js
@@ -1,15 +1,48 @@
 import React from 'react';
-// import API from '../../utils/API';
-// import $ from 'jquery';, { useState, useEffect } 
+import API from '../../utils/API';
+import $ from 'jquery';
+// import FocusContext from '../../utils/FocusContext';, { useState, useEffect }
 
-// ::::   ALL FORMS  :::: //
-// return loggedIn ? <form> : "Component Not Available"
+function NewThreadForm({loginData}) {
 
-function NewThreadForm() {
+    let topic_ids = []
+
+    function generateTopicOptions(){
+        let displayOptions = [];
+        API.findTopics().then(result => {
+            result.data.forEach(topic => {
+                // displayOptions.push( <> <div className="custom-control custom-checkbox">
+                // <input type="checkbox" className="custom-control-input" id="customCheck1">
+                // <label className="custom-control-label" for="customCheck1">"TOPICS"</label>
+                // </div> </>)
+                displayOptions.push(topic.title)
+            });
+        }) 
+
+        return displayOptions
+    }
+
+    function validate(data){
+        // title, min 1 topic, body
+        return (data.title !== '' && data.summary !== '' && data.topics.length >= 1) ? true : false
+    }
+
     function submitThread(e){
-        console.log("==click==\nSUBMIT NEW THREAD");
+        let inputs = {
+            title: $("#thread-title").val(),
+            stance: $("#thread-stance").val(),
+            summary: $("#thread-body").val(),
+            user_id: loginData.id,
+            topics: topic_ids
+        }
+        if (validate(inputs)){
+            API.newThread(inputs).then()
+        } else {
+            alert("please privide more data")
+        }
         e.preventDefault();
     }
+
     return (<ul className="form-wrapper border border-warning" key="new">
         <form>
             <h5>New Thread</h5>
@@ -18,14 +51,18 @@ function NewThreadForm() {
                     <input className="form-row form-group col-12" name="thread-title" id="thread-title" placeholder="Question or Statement Title"></input>
                 </div>
                 <div className="form-group col-3">
+                    <label className="form-row col-12">Starting View:</label>
+                    <div className="form-row">
                         <label className="form-check-label form-group col-2" htmlFor="stance-range">Pro</label>
-                        <input type="range" className="form-group col-6" id="res-stance" min="1" max="5" defaultValue="3" />
+                        <input type="range" className="form-group col-6" id="thread-stance" min="1" max="5" defaultValue="3" />
                         <label className="form-check-label form-group col-2" htmlFor="stance-range">Con</label>
+                    </div>
                 </div>
             </div>
             <div className="form-row">
                 <textarea className="form-group col-12" rows="4" placeholder="Further Context..." name="thread-body" id="thread-body"></textarea>
             </div>
+            {generateTopicOptions()}
         <button onClick={e => submitThread(e)}>Submit</button>
         </form>
     </ul>)
