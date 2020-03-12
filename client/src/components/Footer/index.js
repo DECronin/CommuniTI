b38f221958f696766e76f7b6e3ffc39163150752
Dana Cronin <cronin43224235@gmail.com>
2020-03-12 01:02:26 -0400

    footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import{FaGithub, FaEnvelope} from 'react-icons/fa'
 
 function Footer(){
-    return(<footer className="row">
-        <p className="col-4"></p>
-        <p className="col-4 d-flex justify-content-center">FOOTER</p>
-        <p className="col-4"></p>
-    </footer>)
+    return(<><footer className="footer row">
+        <p className="col-4 d-flex justify-content-center">
+            <a href="https://github.com/DECronin/CommuniTI" target="_blank">
+                <FaGithub style={{ margin: '0 5px' }} />
+            </a> Source Code</p>
+        <p className="col-4 d-flex justify-content-center">Communi.T.I. | 2020</p>
+        <p className="col-4 d-flex justify-content-center">
+        <a href="mailto: decronintech@gmailcom" target="_blank">
+                <FaEnvelope style={{ margin: '0 5px' }} />
+            </a> decronintech@gmail.com
+        </p>
+    </footer></>)
 }
 
-export default Footer
+export default Footer;
